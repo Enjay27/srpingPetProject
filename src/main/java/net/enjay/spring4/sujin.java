ac5127abbc4a5a4c088760804168e6b668b79793
jj <jj@DESKTOP-Q8C2A71>
2020-10-03 15:28:26 +0900

    fd

--- src/main/java/net/enjay/spring4/sujin.java
+++ src/main/java/net/enjay/spring4/sujin.java
@@ -3,6 +3,6 @@ package net.enjay.spring4;
 public class sujin {
 	public static void main( String[] args )
     {
-        System.out.println( "World!" );
+        System.out.println( "dfd vcvxddWdasdasorld!" );
     }
 }
